16f929f086cf0f644cdc392d752f8bb02015113b
rha8xh <rha8xh@virginia.edu>
2026-03-09 19:57:02 -0400

    Building popup expands and collapses

--- game_ui/building_popup.gd
+++ game_ui/building_popup.gd
@@ -1,17 +1,17 @@
 extends Control
 
 @onready var panel : Panel = $BuildingPopupPanel
-@onready var build_button : TextureButton = $BuildingPopupPanel/BuildButton
 
-const COLLAPSED_SIZE = Vector2(0, 0)
-const EXPANDED_SIZE = Vector2(200, 400)
+const COLLAPSED_SIZE = Vector2(1, 1)
+const EXPANDED_SIZE = Vector2(2, 4)
+const COLLAPSED_ROUNDING = 32
+const EXPANDED_ROUNDING = 8
 const TWEEN_DURATION = 0.5
 
 var is_expanded = false
 
 func _ready() -> void:
-	panel.grow_horizontal = Control.GROW_DIRECTION_BEGIN
-	panel.grow_vertical = Control.GROW_DIRECTION_BEGIN
+	pass
 
 
 func _process(delta: float) -> void:
@@ -22,6 +22,7 @@ func _on_build_button_pressed() -> void:
 	is_expanded = !is_expanded
 	_animate_panel(is_expanded)
 	
+	
 
 func _animate_panel(expanding : bool) -> void:
 	var tween = create_tween()
@@ -30,6 +31,8 @@ func _animate_panel(expanding : bool) -> void:
 	tween.set_trans(Tween.TRANS_QUINT)
 	
 	var new_size = EXPANDED_SIZE if expanding else COLLAPSED_SIZE
-	tween.tween_property(panel, "size", new_size, TWEEN_DURATION)
-	tween.tween_callback(panel.queue_free)
+	tween.tween_property(panel, "scale", new_size, TWEEN_DURATION)
+	
+	var new_rounding = EXPANDED_ROUNDING if expanding else COLLAPSED_ROUNDING
+	panel.get_theme_stylebox("panel").set_corner_radius_all(new_rounding)
 	
