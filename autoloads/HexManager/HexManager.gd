66b620164f47598fcc13cda2c8db9bc4871f8c80
Aarfdark <aadarsh.natarajan@gmail.com>
2026-03-09 20:05:30 -0400

    tiles have hex elements yipeeeeeeeeeeeee

--- autoloads/HexManager/HexManager.gd
+++ autoloads/HexManager/HexManager.gd
@@ -13,6 +13,12 @@ const SE = Vector2(1,-1)
 const SOUTH = Vector2(0,-1)
 const SW = Vector2(-1,0)
 
+enum HexElement {
+  EARTH,
+  WOOD,
+  WATER,
+}
+
 ## Preloaded hex packed scenes for instantiating hexes
 ## These should be preloaded constants but preloading them causes a cyclic error.
 var BASE_HEX : PackedScene = load("res://hexes/base/base_hex.tscn")
