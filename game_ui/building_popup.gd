e56e5b2388d74603624694b9c3ce3f183bd5168b
rha8xh <rha8xh@virginia.edu>
2026-03-13 17:00:58 -0400

    Added scroll bar and buttons

--- game_ui/building_popup.gd
+++ game_ui/building_popup.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var panel : Panel = $BuildingPopupPanel
+@onready var scroll_bar : ScrollContainer = $BuildScrollContainer
 
 const COLLAPSED_SIZE = Vector2(1, 1)
 const EXPANDED_SIZE = Vector2(2, 4)
@@ -10,17 +11,15 @@ const TWEEN_DURATION = 0.5
 
 var is_expanded = false
 
+
 func _ready() -> void:
-	pass
-
-
-func _process(delta: float) -> void:
-	pass
+	scroll_bar.modulate.a = 0
 
 
 func _on_build_button_pressed() -> void:
 	is_expanded = !is_expanded
 	_animate_panel(is_expanded)
+	_toggle_scroll_bar(is_expanded)
 	
 	
 
@@ -35,4 +34,21 @@ func _animate_panel(expanding : bool) -> void:
 	
 	var new_rounding = EXPANDED_ROUNDING if expanding else COLLAPSED_ROUNDING
 	panel.get_theme_stylebox("panel").set_corner_radius_all(new_rounding)
+
+
+
+func _toggle_scroll_bar(expanding : bool) -> void:
+	if (expanding):
+		var tween = create_tween()
+		
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_QUINT)
+		
+		scroll_bar.visible = true
+		var new_opacity = 1 if expanding else 0
+		tween.tween_property(scroll_bar, "modulate:a", new_opacity,
+			TWEEN_DURATION).set_delay(0.5 * TWEEN_DURATION).set_delay(0.1 * TWEEN_DURATION)
+	else:
+		scroll_bar.visible = false
+		scroll_bar.modulate.a = 0
 	
