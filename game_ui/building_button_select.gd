d60d894283ff1aa24df77d23553e60aa5b5aaa6e
rha8xh <rha8xh@virginia.edu>
2026-04-10 16:22:57 -0400

    Fixed build menu buttons

--- game_ui/building_button_select.gd
+++ game_ui/building_button_select.gd
@@ -1,6 +1,10 @@
 extends VBoxContainer
 
 func _on_button_toggled(toggled_on: bool, building_tile: String) -> void:
+	var cur_building_name = building_tile.capitalize() + "Button"
+	for button in get_children():
+		if (button.name != cur_building_name):
+			button.set_pressed_no_signal(false)
+	
 	if toggled_on:
 		GameInfo.cur_selected_tile = building_tile
-		print(GameInfo.cur_selected_tile)
