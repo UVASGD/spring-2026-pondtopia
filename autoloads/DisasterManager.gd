1a9348e24bfbb6a8375bc75a2d4b3a0e3a59584e
Aarfdark <aadarsh.natarajan@gmail.com>
2026-04-10 16:41:17 -0400

    flood should be working! don't even worry about the time complexity

--- autoloads/DisasterManager.gd
+++ autoloads/DisasterManager.gd
@@ -1,16 +1,25 @@
 extends Node
 
-const FLOOD_CHANCE = 0.3 #randf()
+const FLOOD_CHANCE = 0.5
 
 func smite_those_frogs(cur_disaster: String):
 	match cur_disaster:
 		"flood":
+			var defended = []
 			for hex in HexManager.hex_list:
-				#hex.send_defend_tiles()
-				print(hex.get_script().get_global_name())
-				if randf() < FLOOD_CHANCE: #destroy it
-					pass
-				
+				if hex.get_script().get_global_name() == "DamHex":
+					for defended_hex in hex.send_defend_tiles():
+						defended.append(defended_hex)
+			
+			var at_risk = []
+			for hex in HexManager.hex_list:
+				if hex not in defended:
+					at_risk.append(hex)
+			for hex in at_risk:
+				var temp = randf()
+				if temp < FLOOD_CHANCE:
+					hex.remove_from_map()
+			
 		"fire":
 			pass
 		"earthquake":
