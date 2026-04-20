38c6a157424b71606420d6085f6ff2e76be2e70f
Aarfdark <aadarsh.natarajan@gmail.com>
2026-04-20 19:16:10 -0400

    temp meteor thing

--- autoloads/DisasterManager.gd
+++ autoloads/DisasterManager.gd
@@ -3,9 +3,9 @@ extends Node
 const FLOOD_CHANCE = 0.5
 
 func smite_those_frogs(cur_disaster: String):
+	var defended = []
 	match cur_disaster:
 		"flood":
-			var defended = []
 			for hex in HexManager.hex_list:
 				if hex.get_script().get_global_name() == "DamHex":
 					for defended_hex in hex.send_defend_tiles():
@@ -25,4 +25,15 @@ func smite_those_frogs(cur_disaster: String):
 		"earthquake":
 			pass
 		"meteor":
-			pass
+			for hex in HexManager.hex_list:
+				if hex.get_script().get_global_name() == "DamHex":
+					for defended_hex in hex.send_defend_tiles():
+						defended.append(defended_hex)
+			
+			var at_risk = []
+			for hex in HexManager.hex_list:
+				if hex not in defended:
+					at_risk.append(hex)
+			for hex in at_risk:
+				hex.remove_from_map()
+			 
